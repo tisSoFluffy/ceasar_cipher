451784914f70e32b8c37df8da37a4bf58c850656
Josh <joshua.l.fritz@gmail.com>
2017-11-30 20:21:10 -0600

    Ceasar Cipher, not quite there yet.

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,5 +1,17 @@
-#start
+ALPHABET_SIZE = 26
 
-def ceasar_cipher(message, shift)
-  alphabet = [*('a'..'z')]
+def caesar_cipher(string)
+  shiftyArray = []
+  charLine = string.chars.map(&:ord)
+
+  shift = 1
+  ALPHABET_SIZE.times do |shift|
+    shiftyArray << charLine.map do |c|
+      ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+    end.join
+  end
+
+  shiftyArray
 end
+
+puts caesar_cipher("Testing")
